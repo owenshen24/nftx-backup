3df1d2964a2607adeddeb08036a4f4190f9c9b59
Owen <owenshen24@gmail.com>
2021-02-14 01:35:53 -0800

    add home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import BigNumber from 'bignumber.js';
+import { ConstantsService } from '../constants.service';
+import { ContractService } from '../contract.service';
+import { UtilsService } from '../utils.service';
+import { WalletService } from '../wallet.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +12,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  vaultIds: any;
 
-  ngOnInit(): void {
+  constructor(public wallet: WalletService, public contract: ContractService, public constants: ConstantsService, public utils: UtilsService)  { 
+    this.resetData();
   }
 
+  ngOnInit(): void {
+    if (this.wallet.connected) {
+      this.loadData();
+    }
+    this.wallet.connectedEvent.subscribe(() => {
+      this.loadData();
+    });
+    this.wallet.errorEvent.subscribe(() => {
+      this.resetData();
+    });
+  }
+
+  resetData() {
+    this.vaultIds = [];
+  }
+
+  async loadData() {
+    let numVaults = parseInt(await this.contract.XSTORE.methods.vaultsLength().call());
+    this.vaultIds = [...Array(numVaults).keys()];
+  }
 }
